b79ffe5b1bc9fdd63321826aaefab9d253718f94
MrAchawin <achawin2549@gmail.com>
2026-09-26 21:02:07 +0700

    add

--- Pokemon/Pokemon.py
+++ Pokemon/Pokemon.py
@@ -2,7 +2,7 @@ from database import get_products
 from components import render_category
 
 
-def open_pokemon_category(root, on_select, on_back, reserved_quantities=None):
+def pokemon_category(root, on_select, on_back, reserved_quantities=None):
     return render_category(
         root, "POKEMON TCG", "#38bdf8", get_products("Pokemon"),
         reserved_quantities, on_select, on_back,
--- Vangurd/Vangurd.py
+++ Vangurd/Vangurd.py
@@ -2,7 +2,7 @@ from database import get_products
 from components import render_category
 
 
-def open_vangurd_category(root, on_select, on_back, reserved_quantities=None):
+def vangurd(root, on_select, on_back, reserved_quantities=None):
     return render_category(
         root, "CARDFIGHT!! VANGUARD", "#f43f5e", get_products("Vanguard"),
         reserved_quantities, on_select, on_back,
